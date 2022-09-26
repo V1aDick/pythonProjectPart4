909be3eb39bc498f7bee5de49f818c865f2d8f8c
V1Dick <djecom.20@mail.ru>
2022-09-26 15:08:07 +0500

    x

--- SoloProject/SoloProject/settings.py
+++ SoloProject/SoloProject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'pages',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -126,4 +128,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
